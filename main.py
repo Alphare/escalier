4a59beb05aec5cd2e9b8865d3ae97f55706c35c2
Raphaël Gomès <alphare33@gmail.com>
2017-01-01 16:23:05 +0100

    Fix the general state of the game and the "4 am programming" dumb things

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from models import GameState, Player
 
-
 def game_loop():
     game_on = True
     game_state = GameState()
-    game_state.players = [Player() for _ in range(0, 5)]
+    game_state.players = [Player() for _ in range(0, 1)]
 
     while (game_on and game_state.number_of_cards < 11):  # Rounds
-        number_of_tricks = range(1, game_state.number_of_cards + 1)
-        game_state.reset()
         game_state.deal()
         game_state.place_bets()
 
-        for _ in number_of_tricks:
+        for _ in range(0, game_state.number_of_cards):
             game_state.play_trick()
 
         game_state.compute_scores()
 
         game_state.number_of_cards += 1
+
+
+if __name__ == '__main__':
+
+    game_loop()
--- models.py
+++ models.py
@@ -24,52 +24,72 @@ class Player:
     cards = attr.ib(default=None)
     bet = attr.ib(default=0)
 
-    def _select_bet_number(self, maximum):
-        pass
+    def _select_bet_number(self, max_number):
+        number = -1
+        while number < 0 or number > max_number:
+            number = int(input('Please enter your bet:\n'))
+        return number
 
-    def place_bet(self, game_state):
-        self._select_bet_number(game_state.number_of_cards)
+    def place_bet(self, max_number):
+        print("You've been dealt {} cards\n=====================".format(max_number))
+        print(", ".join([str(card) for card in self.cards]))
+        self.bet = self._select_bet_number(max_number)
 
     def _choose_card(self, available_cards):
-        pass
+        number = -1
+        while number < 0 or number > len(available_cards):
+            print('Available cards\n===============')
+            print(", ".join([str(card) for card in available_cards]))
+            number = int(input('Please choose your card:\n'))
+        chosen_card = available_cards[number]
+        self.cards.remove(chosen_card)
+        return chosen_card
 
     def choose_card(self, trick_color, super_card_color):
         if trick_color is not None:
             trick_color_cards = get_cards_of_colors(self.cards, [trick_color])
             if trick_color_cards == []:
                 super_card_color_cards = get_cards_of_colors(self.cards, [super_card_color])
-                if super_card_color_cards == []:
-                    self._choose_card(self.cards)
-                else:
-                    self._choose_card(super_card_color_cards)
+                if super_card_color_cards != []:
+                    return self._choose_card(super_card_color_cards)
             else:
-                self._choose_card(trick_color_cards)
-        else:
-            self._choose_card(self.cards)
+                return self._choose_card(trick_color_cards)
+
+        return self._choose_card(self.cards)
+
+    def __str__(self):
+        return self.profile.name
 
 
 @attr.s
 class GameState:  # TODO split into GameState/RoundState/TrickState
-    number_of_cards = attr.ib()
-    super_card = attr.ib()
-    trick_color = attr.ib()
-    played_cards = attr.ib()
-    tricks_per_player = attr.ib()
-    players = attr.ib()
-    deck = attr.ib()
+    number_of_cards = attr.ib(default=1)
+    super_card = attr.ib(default=None)
+    trick_color = attr.ib(default=None)
+    played_cards = attr.ib(default=None)
+    tricks_per_player = attr.ib(default=None)
+    players = attr.ib(default=None)
+    deck = attr.ib(default=None)
 
     def compute_scores(self):
         for index, player in enumerate(self.players):
             player_tricks_count = self.tricks_per_player[index]
-            player.score += sum(player_tricks_count)
+            player.score += player_tricks_count
             if player.bet == player_tricks_count:
                 player.score += 10
+
+        for player in self.players:
+            print("**********")
+            print("SCOREBOARD")
+            print("**********")
+            print("{}: {}".format(player, player.score))
+            print("**********")
 
     def _get_best_card(self, card, current_best_card):
         """
         Assumes cards are of the same color
         """
-        if VALUES.index(card) > VALUES.index(current_best_card):
+        if VALUES.index(card.value) > VALUES.index(current_best_card.value):
             return card
         else:
             return current_best_card
@@ -78,6 +98,7 @@ class GameState:  # TODO split into GameState/RoundState/TrickState
         super_card_color = self.super_card.color
         possible_winning_cards = get_cards_of_colors(self.played_cards,
                                                      (super_card_color, self.trick_color))
+
         current_best_card = possible_winning_cards.pop(0)  # Minor optimization
         for card in possible_winning_cards:
             if card.color == super_card_color:
@@ -96,12 +117,15 @@ class GameState:  # TODO split into GameState/RoundState/TrickState
                     if not current_best_card.color == self.trick_color:
                         current_best_card = self._get_best_card(card, current_best_card)
 
-        return current_best_card.player
+        trick_winner = current_best_card.player
+        print("=================")
+        print('{} wins the trick with a {}'.format(trick_winner, current_best_card))
+        return trick_winner
 
     def deal(self):
         self.deck = Deck()
-        self.played_cards = []
         self.tricks_per_player = [0 for _ in self.players]
+        self.super_card = self.deck.pick()
         for player in self.players:
             player.cards = []
             for card_count in range(0, self.number_of_cards):
@@ -109,35 +133,40 @@ class GameState:  # TODO split into GameState/RoundState/TrickState
                 picked_card.player = player
                 player.cards.append(picked_card)
 
-            self.super_card = self.deck.pick()
 
     def play_trick(self):
+        self.trick_color = None
+        self.played_cards = []
         for index, player in enumerate(self.players):
             chosen_card = player.choose_card(self.trick_color, self.super_card.color)
 
             if self.trick_color is None:
                 self.trick_color = chosen_card.color
 
-            self.played_cards.append(chosen_card)
+            self.add_card_to_played_cards(player, chosen_card)
 
-            if index == len(self.players) - 1:  # Last iteration
-                trick_winner_index = self.players.index(self.get_trick_winner)
-                self.tricks_per_player[trick_winner_index] += 1
+        trick_winner_index = self.players.index(self.get_trick_winner())
+        self.tricks_per_player[trick_winner_index] += 1
+
 
     def place_bets(self):
         for player in self.players:
-            player.place_bet()
+            player.place_bet(self.number_of_cards)
+
+
+    def add_card_to_played_cards(self, player, card):
+        print("{} played a {}".format(player, card))
+        self.played_cards.append(card)
 
 
 @attr.s
 class Card:
     color = attr.ib()
     value = attr.ib()
-    player = attr.ib()
+    player = attr.ib(default=None)
 
-    @property
-    def verbose_name(self):
-        return "{} of {}".format(self.color, self.value)
+    def __str__(self):
+        return "{} of {}".format(self.value, self.color)
 
 
 class Deck:
@@ -153,4 +182,4 @@ class Deck:
         random.shuffle(self.cards)
 
     def pick(self):
-        return self.cards.pop(random.choice(0, len(self.cards)))
+        return self.cards.pop()
